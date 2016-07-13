a1608e1e443be49446fa42412b9248f89e72776c
Sriharsha Setty <sriharsha.setty@onehealth.solutions>
2016-07-13 06:03:35 +0000

    - Migrated to Twix.js

--- expand.js
+++ expand.js
@@ -5,16 +5,123 @@ var moment = require('moment');
 
 var   _    = require('lodash');
 
-var iter = moment("2016-07-07 00:00 +0530", "YYYY-MM-DD HH:mm Z")
-	   .twix("2016-07-07 23:59 +0530", "YYYY-MM-DD HH:mm Z")
+var slots = [],
+    startTime,
+    endTime,
+    am = [
+    [
+	{
+	    "start": 10,
+	    "end": 13
+	},
+	{
+	    "start": 16,
+	    "end": 20
+	}
+    ],
+    [
+	{
+	    "start": "10:00",
+	    "end": "13:00"
+	},
+	{
+	    "start": "16:00",
+	    "end": "20:00"
+	}
+    ],
+    [
+	{
+	    "start": "10:00",
+	    "end": "13:00"
+	},
+	{
+	    "start": "16:00",
+	    "end": "20:00"
+	}
+    ],
+    [
+	{
+	    "start": "10:00",
+	    "end": "13:00"
+	},
+	{
+	    "start": "16:00",
+	    "end": "20:00"
+	}
+    ],
+    [
+	{
+	    "start": "10:00",
+	    "end": "13:00"
+	},
+	{
+	    "start": "16:00",
+	    "end": "20:00"
+	}
+    ],
+
+    [
+	{
+	    "start": "10:00",
+	    "end": "13:00"
+	},
+	{
+	    "start": "16:00",
+	    "end": "20:00"
+	}
+    ],
+	[
+	{
+	    "start": "10:00",
+	    "end": "13:00"
+	},
+	{
+	    "start": "16:00",
+	    "end": "20:00"
+	}
+    ]
+];
+
+function getAvailability(a, d) {
+    var day,
+	start,
+	end,
+	block,
+	blockArray = [];
+
+    day = _.nth(a, 0);
+    for(var i = 0; i < day.length; i++) {
+
+	start = moment(d).add(day[i].start, 'hours');
+	end   = moment(d).add(day[i].end, 'hours');
+
+	blockArray.push(moment(start).twix(end));
+    }
+    return blockArray;
+}
+
+startTime = moment.tz("2016-07-07 00:00", "Asia/Kolkata");
+endTime   = moment.tz("2016-07-07 23:59", "Asia/Kolkata");
+
+var iter = moment(startTime)
+	   .twix(endTime)
 	   .iterate(15, 'minutes');
 
-var slots = [];
+var workingSlots = getAvailability(am, startTime);
 
 while (iter.hasNext()) {
-    slots.push(iter.next()
-	       .utcOffset(330)
-	       .format());
+    var dt = iter.next();
+    if(workingSlots[0].contains(dt) || workingSlots[1].contains(dt)) {
+	slots.push(dt.format());
+    }
 }
 
 console.log(slots);
+
+/* TODOs:
+   - Represent time in local time - for now +5:30 - DONE
+   - Merge  availability matrix (2)
+   - Merge appointments (3)
+   - Merge blocked dates
+   - Merge location identifier
+*/
